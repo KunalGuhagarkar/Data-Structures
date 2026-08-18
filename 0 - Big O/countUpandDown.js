4e1ad861c89eb091b90b440d5a9bd504a31bf598
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-18 05:48:16 +0000

    18 Aug 2026, 11:18

--- 0 - Big O/countUpandDown.js
+++ 0 - Big O/countUpandDown.js
@@ -1,3 +1,12 @@
-function countUpandDown() {
-    
+function countUpandDown(n) {
+    console.log("Going UP");
+    for (let i = 0; i <= n; i++ ) {
+        console.log(i);
+    }
+    console.log('Going DOWN');
+    for (let j = n; j >= 0; j--) {
+        console.log(j);
+    }
 }
+
+countUpandDown();
