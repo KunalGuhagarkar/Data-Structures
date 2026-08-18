c90677347fc7c41cd19450519bff3c5167245221
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-18 05:55:54 +0000

    18 Aug 2026, 11:25

--- 0 - Big O/printAllPairs.js
+++ 0 - Big O/printAllPairs.js
@@ -1,3 +1,10 @@
-function printAllPairs() {
-    
+function printAllPairs(n) {
+    console.log('Start');
+    for (let i = 0; i <= n; i++) {
+        for (let j = 0; j <= n; j++) {
+            console.log(i, j);
+        }
+    }
 }
+
+printAllPairs(5);
