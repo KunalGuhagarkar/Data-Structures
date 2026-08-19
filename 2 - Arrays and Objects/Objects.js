1a4e2d5694e96e32cccb1ab38739a75daecb7b2a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-19 06:14:09 +0000

    19 Aug 2026, 11:44

--- 2 - Arrays and Objects/Objects.js
+++ 2 - Arrays and Objects/Objects.js
@@ -1,7 +1,10 @@
 let instructor = {
     firstName: "Kunal",
     isInstructor: true,
-    favNumbers: [13, 7, 23, 30, 17]
-}
+    favNumbers: [13, 7, 23, 30, 17],
+};
 
+console.log(Object.keys(instructor));
+console.log(Object.values(instructor));
+console.log(Object.entries(instructor));
 console.log(instructor.hasOwnProperty("firstName"));
