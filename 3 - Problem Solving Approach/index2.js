9f1d6cd2b4faf0a019c80af724b83c162250e273
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-21 06:20:10 +0000

    21 Aug 2026, 11:50

--- 3 - Problem Solving Approach/index2.js
+++ 3 - Problem Solving Approach/index2.js
@@ -2,9 +2,15 @@
 
 // Write a function that takes in a string and returns a counts of each character in a string.
 
-// Examples:
+// Simple Examples:
 charCount("aaaa") // {a: 4}
 
 charCount("hello") // {h: 1, e: 1, l: 2, o: 1}
 
+// Complex Examples:
+charCount("my phone number is 182763")
+// are we gonna put numbers, special characters, spaces ?
+
+charCount("Hello hi")
+// are we gonna put capital H and small h seperate or together ?
 
