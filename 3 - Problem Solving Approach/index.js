7e59edd7405b71618cbb36eac0e75ec868a97f0d
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-21 06:14:29 +0000

    21 Aug 2026, 11:44

---
+++ 3 - Problem Solving Approach/index.js
@@ -0,0 +1,21 @@
+// Step 1: Understand the Problem
+
+// Write a function that takes two numbers and returns their sum.
+
+// 1. Can I restate the problem in my own words.
+"implement addition"
+
+// 2. What are the inputs that go into the problem?
+// - ints?
+// - floats?
+// - what about strings for large numbers?
+
+// 3. What are the outputs that should come from the solution to the problem?
+// - int? float? string?
+
+// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem? (You may not be able to answer this question until you set about solving the problem. That's okay; it's still worth considering the question at this early stage.)
+// What if someone only passes only one number ? do we add 0? or undefined or null? 
+// ask the interviewer
+
+// 5. How should I label the important pieces of data that are a part of the problem?
+// like name the function 'add' and the two numbers 'num1' and 'num2' and what returns is 'sum'
---
+++ 3 - Problem Solving Approach/index2.js
@@ -0,0 +1,1 @@
+// Write a function that takes in a string and returns a counts of each character in a string.
