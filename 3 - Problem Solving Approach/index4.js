3f5d36fd97ec4f8859c17dfb90ba4cea843765d4
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-21 06:55:51 +0000

    21 Aug 2026, 12:25

--- 3 - Problem Solving Approach/index4.js
+++ 3 - Problem Solving Approach/index4.js
@@ -1,13 +1,16 @@
 function charCount(str) {
     // make object return at end
     let result = {};
+
     // loop over string, for each chareacter ...
     for (let i = 0; i < str.length; i++) {
         let char = str[i];
+
         // if char is a number/letter AND in object, add 1 to count.
         if (result[char] > 0) {
             result[char]++;
         }
+        
         // if char is a number/letter AND not in object, add it to the object and set value to 1.
         else {
             result[char] = 1;
