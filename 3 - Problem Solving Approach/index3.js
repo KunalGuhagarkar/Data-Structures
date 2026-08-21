6bf4907bc79135b0b9b582792b6dffc650b01239
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-21 06:39:04 +0000

    21 Aug 2026, 12:09

--- 3 - Problem Solving Approach/index3.js
+++ 3 - Problem Solving Approach/index3.js
@@ -2,9 +2,17 @@
 
 
 // skeleton
-function chatCount(str) {
+function charCount(str) {
     // do something
     // return an object with keys that are lowercase alphanumeric characters in the string; values should be counts for those characters
+}
 
-    
+function charCount(str) {
+    // make object return at end
+    // loop over string, for each chareacter ...
+        // if char is a number/letter AND in object, add 1 to count.
+        // if char is a number/letter AND not in object, add it to the object and set value to 1.
+        // if char is something else (space, period, etc) don't do anyting
+
+    // return object at end
 }
