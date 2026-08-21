12a93fd70aa031811f070b4fa15496d354c5425c
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-21 06:55:28 +0000

    21 Aug 2026, 12:25

--- 3 - Problem Solving Approach/index4.js
+++ 3 - Problem Solving Approach/index4.js
@@ -1,3 +1,21 @@
 function charCount(str) {
-    
+    // make object return at end
+    let result = {};
+    // loop over string, for each chareacter ...
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        // if char is a number/letter AND in object, add 1 to count.
+        if (result[char] > 0) {
+            result[char]++;
+        }
+        // if char is a number/letter AND not in object, add it to the object and set value to 1.
+        else {
+            result[char] = 1;
+        }
+    }
+
+    // if char is something else (space, period, etc) don't do anyting
+
+    // return object at end
+    return result;
 }
