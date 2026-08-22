d95f9ad09891087f27d04b8a3400b9a75f89a628
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-22 08:13:34 +0000

    22 Aug 2026, 13:43

--- 4 - Problem Solving Patterns/index.js
+++ 4 - Problem Solving Patterns/index.js
@@ -1,1 +1,18 @@
 // Practice
+
+function same(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+
+    for(let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+
+    
+}
