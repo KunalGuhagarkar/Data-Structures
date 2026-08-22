c12d5f23b3d70d4bdeb3a153aa03d079ac6eed9a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-22 07:18:58 +0000

    22 Aug 2026, 12:48

--- 4 - Problem Solving Patterns/index.js
+++ 4 - Problem Solving Patterns/index.js
@@ -8,15 +8,40 @@
     same([1,2,1], [4,4,1]) // false (must be same frequency)
 */
 
-function same(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
+// Time Complexity => O(n^2)
+// function same(arr1, arr2) {
+//     if (arr1.length !== arr2.length) return false;
 
-    for (let i = 0; i < arr1.length; i++) {
-        let correctIndex = arr2.indexOf(arr1[i] ** 2);
-        if (correctIndex === -1) return false;
-        arr2.splice(correctIndex, 1);
+//     for (let i = 0; i < arr1.length; i++) {
+//         let correctIndex = arr2.indexOf(arr1[i] ** 2);
+//         if (correctIndex === -1) return false;
+//         arr2.splice(correctIndex, 1);
+//     }
+//     return true;
+// }
+
+// console.log(same([1,2,3], [4,1,9]));
+
+// Using Frequency Counter Pattern
+// Time Complexity => O(n)
+
+function same(arr1, arr2) {
+    if (arr1.length != arr2.length) return false;
+
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    for (val in arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
     }
-    return true;
+
+    for (val in arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+
+    console.log(frequencyCounter1);
+    console.log(frequencyCounter2);
 }
+
 
 console.log(same([1,2,3], [4,1,9]));
