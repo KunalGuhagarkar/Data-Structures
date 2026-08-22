4e7b2b074bd6ccc64a164c05f26132832a3f9202
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-22 06:24:00 +0000

    22 Aug 2026, 11:54

--- 4 - Problem Solving Patterns/index.js
+++ 4 - Problem Solving Patterns/index.js
@@ -10,6 +10,12 @@
 
 function same(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2);
+        if (correctIndex === -1) return false;
+    }
+    arr1.splice(correctIndex, 1);
 }
 
 console.log(same([1,2], [1]));
