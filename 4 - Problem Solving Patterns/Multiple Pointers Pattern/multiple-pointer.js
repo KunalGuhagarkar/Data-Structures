addf2efd8299fd5177525a05d79fd44e4eae67e0
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-24 07:08:52 +0000

    24 Aug 2026, 12:38

--- 4 - Problem Solving Patterns/Multiple Pointers Pattern/multiple-pointer.js
+++ 4 - Problem Solving Patterns/Multiple Pointers Pattern/multiple-pointer.js
@@ -11,7 +11,6 @@
 function sumZero(arr) {
     for(let i = 0; i < arr.length; i++) {
         for(let j = arr.length-1; j >= 0; j--) {
-            console.log(arr[i], arr[j]);
             if(arr[i] + arr[j] === 0) {
                 return [arr[i], arr[j]];
             }
