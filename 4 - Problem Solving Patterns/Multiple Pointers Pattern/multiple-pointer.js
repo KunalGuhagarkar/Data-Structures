99d6ac542574c43cc443d84484ca51902d0d08f8
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-24 07:27:20 +0000

    24 Aug 2026, 12:57

--- 4 - Problem Solving Patterns/Multiple Pointers Pattern/multiple-pointer.js
+++ 4 - Problem Solving Patterns/Multiple Pointers Pattern/multiple-pointer.js
@@ -8,17 +8,30 @@
 // sumZero([1,2,3]) // undefined
 
 // Brute Force Solution:
+// function sumZero(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === 0) {
+//                 return [arr[i], arr[j]];
+//             }
+//         }
+//     }
+//     return undefined;
+// }
+
+// Multiple Pointer Solution
 function sumZero(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === 0) {
-                return [arr[i], arr[j]];
-            }
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        const sum = arr[left] + arr[right];
+        if (sum == 0) {
+            return [arr[left], arr[right]];
         }
     }
-    return undefined;
 }
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 4]));
 console.log(sumZero([-2, 0, 1, 3]));
 console.log(sumZero([1, 2, 3]));
