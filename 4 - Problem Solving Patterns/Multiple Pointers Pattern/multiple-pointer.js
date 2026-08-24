c1dbe7e08cba917a3a1db935070fca153a622dc8
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-24 07:10:32 +0000

    24 Aug 2026, 12:40

--- 4 - Problem Solving Patterns/Multiple Pointers Pattern/multiple-pointer.js
+++ 4 - Problem Solving Patterns/Multiple Pointers Pattern/multiple-pointer.js
@@ -3,17 +3,17 @@
 // Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
 
 // Examples
-// sumZero([-3,-2,-1,0,1,2,3]) // [-3,3] 
+// sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
 // sumZero([-2,0,1,3]) // undefined
 // sumZero([1,2,3]) // undefined
 
 // Brute Force Solution:
 function sumZero(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = arr.length-1; j >= 0; j--) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = arr.length - 1; j >= 0; j--) {
             if (arr[i] === arr[j]) return undefined;
-            
-            if(arr[i] + arr[j] === 0) {
+
+            if (arr[i] + arr[j] === 0) {
                 return [arr[i], arr[j]];
             }
         }
@@ -21,5 +21,6 @@ function sumZero(arr) {
     return undefined;
 }
 
-console.log(sumZero([-3,-2,-1,0,1,2,3]))
-console.log(sumZero([-2,0,1,3]));
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([-2, 0, 1, 3]));
+console.log(sumZero([1, 2, 3]));
