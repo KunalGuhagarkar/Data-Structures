7c39ebfae6a569fe0d21b273c20c4f19fc801a0e
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-24 08:28:40 +0000

    24 Aug 2026, 13:58

--- 4 - Problem Solving Patterns/Multiple Pointers Pattern/count-unique-values.js
+++ 4 - Problem Solving Patterns/Multiple Pointers Pattern/count-unique-values.js
@@ -1,5 +1,21 @@
 // Implement a function called **countUniqueValues,** which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
 
+// Example
+// countUniqueValues([1,1,1,1,1,2]) // 2
+// countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+// countUniqueValues([]) // 0
+// countUniqueValues([-2,-1,-1,0,1]) // 4
+
 function countUniqueValues(arr) {
-    
+    let start = 0;
+    let start_plus = 1;
+    let count = 0;
+    while (start_plus < arr.length) {
+        if (arr[start] !== arr[start_plus]) {
+            count++;
+        } else {
+            start++;
+            start_plus++;
+        }
+    }
 }
