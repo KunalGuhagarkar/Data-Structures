866e0e0587c0d5f2f2b0d241375b3330ad8a8a47
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-24 09:28:09 +0000

    24 Aug 2026, 14:58

--- 4 - Problem Solving Patterns/Multiple Pointers Pattern/count-unique-values.js
+++ 4 - Problem Solving Patterns/Multiple Pointers Pattern/count-unique-values.js
@@ -6,20 +6,20 @@
 // countUniqueValues([]) // 0
 // countUniqueValues([-2,-1,-1,0,1]) // 4
 
-// function countUniqueValues(arr) {
-//     let start = 0;
-//     let count = 0;
+function countUniqueValues(arr) {
+    let start = 0;
+    let count = 0;
 
-//     if (arr.length === 0) return count;
+    if (arr.length === 0) return count;
 
-//     while (start < arr.length) {
-//         if (arr[start] !== arr[start + 1]) {
-//             count++;
-//         }
-//         start++;
-//     }
-//     return count;
-// }
+    while (start < arr.length) {
+        if (arr[start] !== arr[start + 1]) {
+            count++;
+        }
+        start++;
+    }
+    return count;
+}
 
 // console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
 // console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
@@ -27,7 +27,7 @@
 // console.log(countUniqueValues([-2, -1, -1, 0, 1]));
 
 // Another Approach
-function countUniqueValues(arr) {
+function countUniqueValues2(arr) {
     if (arr.length === 0) return 0;
 
     let i = 0;
