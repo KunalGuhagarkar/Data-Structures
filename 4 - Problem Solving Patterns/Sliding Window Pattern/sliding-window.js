0acfdc6cfc92ecb8920f15fad16e8ca1c56a46d7
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-25 07:11:26 +0000

    25 Aug 2026, 12:41

--- 4 - Problem Solving Patterns/Sliding Window Pattern/sliding-window.js
+++ 4 - Problem Solving Patterns/Sliding Window Pattern/sliding-window.js
@@ -13,8 +13,17 @@ function maxSubarraySum(arr, num) {
     if (arr.length === 0) return null;
 
     let max = -Infinity;
-    
+
     for(let i = 0; i < arr.length - num + 1; i++) {
-        temp = 0
-    } 
+        temp = 0;
+        for (let j = 0; j < num; j++) {
+            temp += arr[i+j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+    }
+    return max;
 }
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5],2));
