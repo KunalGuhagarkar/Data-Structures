d8a879c1c7291898c9fb7c6441ad61fbfbc694e0
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-26 06:12:43 +0000

    26 Aug 2026, 11:42

---
+++ 4 - Problem Solving Patterns/Practice Challenges/1-sameFrequency.js
@@ -0,0 +1,15 @@
+/*
+Write a function called **sameFrequency.** Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+Your solution MUST have the following complexities:
+
+Time: O(N)
+
+Sample Input:
+
+1. sameFrequency(182,281) // true
+2. sameFrequency(34,14) // false
+3. sameFrequency(3589578, 5879385) // true
+4. sameFrequency(22,222) // false
+
+*/
