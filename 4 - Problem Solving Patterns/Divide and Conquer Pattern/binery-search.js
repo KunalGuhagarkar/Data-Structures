c0e1240fcaff99b5666d3cc3f2d453e2f387eaaa
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-26 06:00:19 +0000

    26 Aug 2026, 11:30

--- 4 - Problem Solving Patterns/Divide and Conquer Pattern/binery-search.js
+++ 4 - Problem Solving Patterns/Divide and Conquer Pattern/binery-search.js
@@ -21,8 +21,8 @@
 function search(arr, target) {
     let min = 0;
     let max = arr.length - 1;
-    
-    while(min < max) {
+
+    while (min < max) {
         const mid = Math.floor((min + max) / 2);
         if (arr[mid] === target) {
             return mid;
@@ -32,6 +32,7 @@ function search(arr, target) {
             min = mid;
         }
     }
+    return -1;
 }
 
 console.log(search([1, 2, 3, 4, 5, 6, 7], 4));
