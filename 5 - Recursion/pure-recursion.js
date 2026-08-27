69a595d31d9bf2e4a876d68a82ad31aaa2f34889
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-27 07:13:11 +0000

    27 Aug 2026, 12:43

--- 5 - Recursion/pure-recursion.js
+++ 5 - Recursion/pure-recursion.js
@@ -6,7 +6,7 @@ function collectOddValues(arr) {
     if (arr.length === 0) return;
 
     // if (arr.splice(arr.length))
-    console.log(arr.splice(0, arr.length));
+    console.log(arr.splice(arr.length-3));
 }
 
 collectOddValues([1,2,3,4,5]);
