4e306278ee8a25460a12d8ff0fbb2b6741843eb0
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-27 05:55:41 +0000

    27 Aug 2026, 11:25

--- 5 - Recursion/factorial.js
+++ 5 - Recursion/factorial.js
@@ -5,13 +5,20 @@
 
 // Interative Approach
 
+// function factorial(num) {
+//     if (num < 0) return undefined;
+//     let fact = 1;
+//     for (let i = num; i > 1; i--) {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+
+// Recursive Approach
+
 function factorial(num) {
-    if (num < 0) return undefined;
-    let fact = 1;
-    for (let i = num; i > 1; i--) {
-        fact *= i;
-    }
-    return fact;
+    if (num === 1) return 1;
+    return num * factorial(num-1);
 }
 
 console.log(factorial(5));
