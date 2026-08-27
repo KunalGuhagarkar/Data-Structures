6017e0b605b91853ebc6c3a589aa0661894c6e0a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-27 05:50:29 +0000

    27 Aug 2026, 11:20

--- 5 - Recursion/factorial.js
+++ 5 - Recursion/factorial.js
@@ -8,9 +8,9 @@
 function factorial(num) {
     let fact = 1;
     for (let i = num; i > 1; i--) {
-        fact *= i;
+        i *= i;
     }
-    return fact;
+    return i;
 }
 
 console.log(factorial(2));
