f8657f08c147c5202d20e8b49014497e37fe6ffc
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-27 10:11:14 +0000

    27 Aug 2026, 15:41

--- 4 - Problem Solving Patterns/Practice Challenges/2-areThereDuplicates.js
+++ 4 - Problem Solving Patterns/Practice Challenges/2-areThereDuplicates.js
@@ -1,6 +1,9 @@
 // Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
 
 function areThereDuplicates(...items) {
-
+    console.log(items);
 }
+
+console.log(areThereDuplicates(1,2,3,4));
+
 
