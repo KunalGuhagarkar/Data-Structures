569acc1482b661c484b39b3f33885fcf58cf53f0
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-27 07:23:20 +0000

    27 Aug 2026, 12:53

--- 5 - Recursion/pure-recursion.js
+++ 5 - Recursion/pure-recursion.js
@@ -3,14 +3,14 @@
 // Collect all of the odd values in an array!
 
 function collectOddValues(arr) {
-    if (arr.length === 0) return 1;
+    if (arr.length === 0) return;
 
     console.log(arr);
 
     if (arr[0] % 2 !== 0) {
-        console.log(arr);
-        return collectOddValues(arr.splice(1));
+        console.log(arr[0]);
     }
+    return collectOddValues(arr.splice(1));
 }
 
 console.log(collectOddValues([1, 2, 3, 4, 5]));
