2749640a4428a65ba8b1fcc9a81b40f49e985611
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-28 06:19:47 +0000

    28 Aug 2026, 11:49

--- 5 - Recursion/Problem Set/power.js
+++ 5 - Recursion/Problem Set/power.js
@@ -2,8 +2,9 @@
 // Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow()  - do not worry about negative bases and exponents.
 
 function power(num, exp) {
+    if (exp === 0) return 1;
     if (exp === 1) return num;
     return num * power(num, exp - 1);
 }
 
-console.log(power(2, 5));
+console.log(power(2, 0));
