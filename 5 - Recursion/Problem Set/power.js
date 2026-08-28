e73a0a67da867e199f66e7b24cfc92dd624f39b9
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-28 06:16:56 +0000

    28 Aug 2026, 11:46

--- 5 - Recursion/Problem Set/power.js
+++ 5 - Recursion/Problem Set/power.js
@@ -4,8 +4,7 @@
 function power(num, exp) {
     let result = 1;
     if (exp < 1) return num-1;
-    console.log(num);
     return num * power(num, exp - 1);
 }
 
-console.log(power(2, 3));
+console.log(power(2, 5));
