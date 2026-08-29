ea9c8beb98860f89c4cee604bab0412a497c22a8
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-29 07:55:37 +0000

    29 Aug 2026, 13:25

---
+++ 6 - Searching Algorithms/searching-in-js.js
@@ -0,0 +1,10 @@
+// JavaScript has search!
+// There are many different search methods on arrays in JavaScript:
+
+// indexOf
+// includes
+// find
+// findIndex
+
+const names = ["Alex", "Jordan", "Taylor", "Morgan", "Sam", "Riley"];
+
