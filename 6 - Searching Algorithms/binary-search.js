1871f2dffe02a81d23417de1b57c7b07a31b3b65
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-29 08:33:17 +0000

    29 Aug 2026, 14:03

---
+++ 6 - Searching Algorithms/binary-search.js
@@ -0,0 +1,10 @@
+// Binary Search
+
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+    }
+}
