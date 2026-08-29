13710688bfec6d05230bcb103de8d9e686261993
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-29 08:38:24 +0000

    29 Aug 2026, 14:08

--- 6 - Searching Algorithms/binary-search.js
+++ 6 - Searching Algorithms/binary-search.js
@@ -2,11 +2,16 @@
 
 // Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
 
-// Examples
+// Examples:
+
 // binarySearch([1,2,3,4,5],2) // 1
+
 // binarySearch([1,2,3,4,5],3) // 2
+
 // binarySearch([1,2,3,4,5],5) // 4
+
 // binarySearch([1,2,3,4,5],6) // -1
+
 // binarySearch([
 //   5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
 //   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
@@ -21,6 +26,8 @@
 //   5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 
 //   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
 // ], 100) // -1
+
+// Solution
 
 function binarySearch(arr, target) {
     let left = 0;
