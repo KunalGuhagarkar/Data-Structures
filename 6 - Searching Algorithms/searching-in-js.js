50485837a485b3a89a8feae797c474e687f1237c
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-29 07:59:00 +0000

    29 Aug 2026, 13:29

--- 6 - Searching Algorithms/searching-in-js.js
+++ 6 - Searching Algorithms/searching-in-js.js
@@ -7,7 +7,12 @@
 // findIndex
 
 const names = ["Alex", "Jordan", "Taylor", "Morgan", "Sam", "Riley"];
+// indexes:      0         1         2         3       4       5
 
 // indexOf
-console.log(names.indexOf('Jordan')); // 1
-console.log(names.indexOf('Kunal'));
+console.log(names.indexOf('Jordan')); // 1 (found at index 1)
+console.log(names.indexOf('Kunal')); // -1 (not found)
+
+// includes
+console.log(names.includes("Morgan"));
+console.log(names.includes("Kunal"));
