d51260884187bd147e61eadbe18fec58f2e84622
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-30 08:22:37 +0000

    30 Aug 2026, 13:52

--- 5 - Recursion/Problem Set/productOfArray.js
+++ 5 - Recursion/Problem Set/productOfArray.js
@@ -14,7 +14,7 @@ function productOfArray(arr) {
 
 // Time Complexity -> O(n)
 function productOfArray2(arr, index=0) {
-    if (index === arr.length-1) return 1;
+    if (index === arr.length) return 1;
     console.log(arr[index]);
     return arr[index] * productOfArray2(arr, index + 1); 
 }
