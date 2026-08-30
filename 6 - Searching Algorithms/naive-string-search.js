226f6306575fffa18b09a766da280f2dfdf2392d
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-30 09:26:01 +0000

    30 Aug 2026, 14:56

--- 6 - Searching Algorithms/naive-string-search.js
+++ 6 - Searching Algorithms/naive-string-search.js
@@ -1,2 +1,18 @@
 // Naive String Search
 
+// Pseudocode
+
+// - Loop over the longer string
+// - Loop over the shorter string
+// - If the characters don't match, break out of the inner loop
+// - If the characters do match, keep going
+// - If you complete the inner loop and find a match, increment the count of matches
+// - Return the count
+
+function naiveStringSearch(str, keyword) {
+    for (let char of str) {
+        console.log(char);
+    }
+}
+
+console.log(naiveStringSearch("kunal", "nal"));
