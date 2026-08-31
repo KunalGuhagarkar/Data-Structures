a5995b5db52ace0e89e568c6d89f305b875d7e9a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-31 08:49:40 +0000

    31 Aug 2026, 14:19

--- 6 - Searching Algorithms/practice.js
+++ 6 - Searching Algorithms/practice.js
@@ -3,10 +3,10 @@
 function naiveStringSearch(long, short) {
     for(let i = 0; i < long.length; i++) {
         for (let j = 0; j < short.length; j++) {
-            if(long[i] !== short[j+i]) {
+            if(long[i+j] !== short[i]) {
                 break;
             }
-            console.log(long[i], short[j+i]);
+            console.log(long[i+j], short[j]);
         }
         console.log("BREAK");
     }
