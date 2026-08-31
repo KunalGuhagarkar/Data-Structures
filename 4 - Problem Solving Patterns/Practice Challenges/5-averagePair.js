10817446f252601ad6af791a581226b37571a91d
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-31 06:32:32 +0000

    31 Aug 2026, 12:02

--- 4 - Problem Solving Patterns/Practice Challenges/5-averagePair.js
+++ 4 - Problem Solving Patterns/Practice Challenges/5-averagePair.js
@@ -21,7 +21,6 @@ function averagePair(arr, target) {
     let second = first + 1;
 
     while (second < arr.length) {
-        console.log((arr[first] + arr[second]) / 2)
         if ((arr[first] + arr[second]) / 2 === target) {
             return true;
         }
@@ -32,3 +31,6 @@ function averagePair(arr, target) {
 }
 
 console.log(averagePair([1, 2, 3], 2.5));
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1));
+console.log(averagePair([], 4));
