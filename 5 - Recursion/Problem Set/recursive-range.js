f8c0db68e9eca77f9811af0da0f3f2d82981f0b9
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-31 07:01:50 +0000

    31 Aug 2026, 12:31

--- 5 - Recursion/Problem Set/recursive-range.js
+++ 5 - Recursion/Problem Set/recursive-range.js
@@ -2,3 +2,12 @@
 
 // Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.
 
+// Example
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+// Solution
+
+function recursiveRange(num) {
+    
+}
