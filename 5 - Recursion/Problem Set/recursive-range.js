b6fdfe109dfb845edb170db30b91dcd1e7183803
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-08-31 07:03:34 +0000

    31 Aug 2026, 12:33

--- 5 - Recursion/Problem Set/recursive-range.js
+++ 5 - Recursion/Problem Set/recursive-range.js
@@ -9,6 +9,8 @@
 // Solution
 
 function recursiveRange(num) {
-    if (num === 1) return;
+    if (num === 1) return 1;
     return num + recursiveRange(num - 1);
 }
+
+console.log(recursiveRange(6));
