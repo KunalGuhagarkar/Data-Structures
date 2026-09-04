1bfde1803c3f0dd72ec31bef46ce70c82a74b65a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-04 14:36:44 +0000

    4 Sept 2026, 20:06

--- 7 - Sorting/2 - Selection Sort/selection-sort.js
+++ 7 - Sorting/2 - Selection Sort/selection-sort.js
@@ -13,9 +13,11 @@ function selectionSort(arr) {
         for (let j = 0; j < arr.length; j++) {
             if (arr[j + 1] < arr[j]) {
                 min = arr[j + 1];
-            }
-            if (arr[i] !== min) {
-                [arr[i], arr[j + 1]] = [arr[j + 1], arr[i]];
+
+                if (arr[i] !== min) {
+                    [arr[i], arr[j + 1]] = [arr[j + 1], arr[i]];
+                    
+                }
             }
         }
     }
