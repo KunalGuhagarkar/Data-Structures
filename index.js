4bd35ae8dd99cd92126c266ad7c45b1f355a51be
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-05 07:36:10 +0000

    5 Sept 2026, 13:06

---
+++ index.js
@@ -0,0 +1,16 @@
+// insertion sort practice
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > currentVal) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = currentVal;
+    }
+    return arr;
+}
+
+console.log(insertionSort([5, 4, 3, 2, 1]));
