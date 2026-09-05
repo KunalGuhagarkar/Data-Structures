902eac350df5ee71a78e25348e25abe05dc6b231
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-05 06:21:29 +0000

    5 Sept 2026, 11:51

--- 7 - Sorting/3 - Insertion Sort/insertion-sort.js
+++ 7 - Sorting/3 - Insertion Sort/insertion-sort.js
@@ -9,8 +9,13 @@
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         let currentVal = arr[i];
-        for (let j = i - 1; j > 0; j--) {}
+        for (let j = i - 1; j >= 0; j--) {
+            if (currentVal < arr[j]) {
+                
+            }
+        }
     }
+    return arr;
 }
 
 console.log(insertionSort([2, 1, 9, 76, 4]));
