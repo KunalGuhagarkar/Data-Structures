dbd4a78e0e16c30a1d7b0437473857d5e8a0c73f
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-05 11:11:53 +0000

    5 Sept 2026, 16:41

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let min = i;
         for (let j = i + 1; j < arr.length; j++) {
-            
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            [arr[min], arr[i]] = [arr[i], arr[min]];
         }
     }
 
