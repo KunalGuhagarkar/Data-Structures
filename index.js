814545b2dd5fc89f23803959efe9373df94c2e95
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-07 06:12:57 +0000

    7 Sept 2026, 11:42

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // bubble sort
 
-function bubbleSort(arr) {
-    for (let i = arr.length; i > 0; i--) {
-        noSwaps = true;
-        for (let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-                noSwaps = false;
-            }
-        }
-        if (noSwaps) break;
-    }
-    return arr;
-}
+// function bubbleSort(arr) {
+//     for (let i = arr.length; i > 0; i--) {
+//         noSwaps = true;
+//         for (let j = 0; j < i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//                 noSwaps = false;
+//             }
+//         }
+//         if (noSwaps) break;
+//     }
+//     return arr;
+// }
 
-console.log(bubbleSort([5, 4, 3, 2, 1]));
+// console.log(bubbleSort([5, 4, 3, 2, 1]));
+
+
+// Selection Sort
