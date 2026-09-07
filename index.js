76c60d53b925be581c974394f98be1c237b8849d
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-07 07:05:57 +0000

    7 Sept 2026, 12:35

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function selectionSort(arr) {
 console.log(selectionSort([5, 4, 3, 2, 1]));
 
 function insertionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
         let currentVal = arr[i];
         let j = i - 1;
         while (j >= 0 && arr[j] > currentVal) {
