e2dc0af95f84a181346898d630d023a7d02fc93c
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-07 06:52:37 +0000

    7 Sept 2026, 12:22

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-//
+// bubble sort
+
 function bubbleSort(arr) {
     let noSwaps;
     for (let i = arr.length; i > 0; i--) {
         noSwaps = true;
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
                 noSwaps = false;
             }
         }
@@ -13,38 +14,3 @@ function bubbleSort(arr) {
     }
     return arr;
 }
-
-console.log(bubbleSort([5, 4, 3, 2, 1]));
-
-function selectionSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        let min = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[min] > arr[j]) {
-                min = j;
-            }
-        }
-        if (min !== i) {
-            [arr[min], arr[i]] = [arr[i], arr[min]];
-        }
-    }
-    return arr;
-}
-
-console.log(selectionSort([5, 4, 3, 2, 1]));
-
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let currentVal = arr[i];
-        let j = i - 1;
-
-        while (j >= 0 && arr[j] > currentVal) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j+1] = currentVal;
-    }
-    return arr;
-}
-
-console.log(insertionSort([2, 4, 3, 5, 1]));
