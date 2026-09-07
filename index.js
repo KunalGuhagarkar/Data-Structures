24e92bf2565788c14d29c8779e625a138cb51e0a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-07 08:13:16 +0000

    7 Sept 2026, 13:43

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ console.log(bubbleSort([5, 4, 3, 2, 1]));
 // selection sort
 
 function selectionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let min = i;
-        for (let j = i-1; j < arr.length; j++) {
-            if (arr[j] > arr[min]) {
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
                 min = j;
             }
         }
