0a528caeea483b935da6f220111593b632868e85
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-08 16:09:58 +0000

    8 Sept 2026, 21:39

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 function bubbleSort(arr) {
     let noSwap;
-    for(let i = arr.length; i > 1; i-- ) {
+    for (let i = arr.length; i > 1; i--) {
         noSwap = true;
         for (let j = 0; j < arr.length; j++) {
-            if (arr[j] > arr[j+1]) {
-                [arr[j+1], arr[j]] = [arr[j], arr[j+1]];
+            if (arr[j] > arr[j + 1]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
                 noSwap = false;
             }
         }
@@ -15,14 +15,14 @@ function bubbleSort(arr) {
     return arr;
 }
 
-console.log(bubbleSort([5,3,2,1]));
+console.log(bubbleSort([5, 3, 2, 1]));
 
 // Selection Sort
 
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         let min = i;
-        for (let j = i+1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[min]) {
                 min = j;
             }
@@ -34,5 +34,21 @@ function selectionSort(arr) {
     return arr;
 }
 
-console.log(selectionSort([5,4,3,2,1]));
+console.log(selectionSort([5, 4, 3, 2, 1]));
 
+// Insertion Sort
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currentValue = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > currentValue) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = currentValue;
+    }
+    return arr;
+}
+
+console.log(insertionSort([5, 4, 3, 2, 1]));
