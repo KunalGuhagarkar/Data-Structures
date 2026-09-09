184e76debabf338a644c27cf49dd51d46ca4ef0a
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-09 05:08:50 +0000

    9 Sept 2026, 10:38

--- 7 - Sorting/4 - Merge Sort/merge-sort.js
+++ 7 - Sorting/4 - Merge Sort/merge-sort.js
@@ -8,3 +8,20 @@
     // - If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
     // - Once we exhaust one array, push in all remaining values from the other array
 
+function mergingArrays(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length || j < arr1.length) {
+        if (arr[i] < arr[j]) {
+            result.push(arr[i]);
+            i++;
+        } else if(arr[j] < arr[i]) {
+            result.push(arr[j]);
+            j++;
+        }
+    }
+}
+
+console.log([1, 10, 50], [2, 14, 99, 100]);
