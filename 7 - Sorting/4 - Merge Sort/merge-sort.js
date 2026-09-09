4740584f624cbfb8b2f8a8181946479783538053
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-09 07:59:25 +0000

    9 Sept 2026, 13:29

--- 7 - Sorting/4 - Merge Sort/merge-sort.js
+++ 7 - Sorting/4 - Merge Sort/merge-sort.js
@@ -6,13 +6,14 @@
 // - Once the array has been merged back together, return the merged (and sorted!) array
 
 // Solution
+import mergingArrays from "./merging-sorted-arrays";
 
 function mergeSort(arr) {
     if (arr.length <= 1) return arr;
     let mid = Math.floor(arr.length / 2);
     let left = mergeSort(arr.slice(0, mid));
     let right = mergeSort(arr.slice(mid));
-    
+    return mergingArrays(left, right);
 }
 
-console.log(mergeSort([10,24,76,73]));
+console.log(mergeSort([10, 24, 76, 73]));
