9eef31584a95de8b4074671a8e272ffd6097633e
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-09 07:50:19 +0000

    9 Sept 2026, 13:20

--- 7 - Sorting/4 - Merge Sort/merging-sorted-arrays.js
+++ 7 - Sorting/4 - Merge Sort/merging-sorted-arrays.js
@@ -1,5 +1,6 @@
 // Merging Arrays
 
+// Pseudocode
 // - Create an empty array, take a look at the smallest values in each input array
 // - While there are still values we haven't looked at...
 // - If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
