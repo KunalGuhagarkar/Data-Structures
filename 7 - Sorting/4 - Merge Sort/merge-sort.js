75ee0858ee894da429ee234b10d3e31064414005
Kunal Guhagarkar Mac <kunal.workspace13@gmail.com>
2026-09-09 05:10:00 +0000

    9 Sept 2026, 10:40

--- 7 - Sorting/4 - Merge Sort/merge-sort.js
+++ 7 - Sorting/4 - Merge Sort/merge-sort.js
@@ -4,9 +4,9 @@
 
 // - Create an empty array, take a look at the smallest values in each input array
 // - While there are still values we haven't looked at...
-    // - If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
-    // - If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
-    // - Once we exhaust one array, push in all remaining values from the other array
+// - If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+// - If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+// - Once we exhaust one array, push in all remaining values from the other array
 
 function mergingArrays(arr1, arr2) {
     let result = [];
@@ -17,11 +17,12 @@ function mergingArrays(arr1, arr2) {
         if (arr1[i] < arr2[j]) {
             result.push(arr1[i]);
             i++;
-        } else if(arr2[j] < arr1[i]) {
+        } else if (arr2[j] < arr1[i]) {
             result.push(arr2[j]);
             j++;
         }
     }
+    return result;
 }
 
 console.log(mergingArrays([1, 10, 50], [2, 14, 99, 100]));
